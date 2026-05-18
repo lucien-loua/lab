7d8332ec3483fbc626bf67cf20cb997f432b0f2d
Lucien Loua <lucien.loua@gmail.com>
2026-05-18 11:04:37 +0000

    chore: mount TooltipProvider in (lab) layout

--- app/(lab)/layout.tsx
+++ app/(lab)/layout.tsx
@@ -1,12 +1,15 @@
 import { SiteFooter } from "@/components/lab/layout/site-footer";
 import { SiteHeader } from "@/components/lab/layout/site-header";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
 export default function LabLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-svh flex-col">
-      <SiteHeader />
-      <div className="flex-1">{children}</div>
-      <SiteFooter />
-    </div>
+    <TooltipProvider delay={250}>
+      <div className="flex min-h-svh flex-col">
+        <SiteHeader />
+        <div className="flex-1">{children}</div>
+        <SiteFooter />
+      </div>
+    </TooltipProvider>
   );
 }
